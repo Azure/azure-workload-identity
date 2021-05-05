6d29d702b880c27dec4229069d79e048f327f0a8
Ernest Wong <chuwon@microsoft.com>
2021-05-04 17:06:06 -0700

    test: e2e test framework backbone (#18)
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

---
+++ test/e2e/e2e_test.go
@@ -0,0 +1,30 @@
+package e2e
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/kubernetes/test/e2e/framework/config"
+)
+
+// handleFlags sets up all flags and parses the command line.
+func handleFlags() {
+	config.CopyFlags(config.Flags, flag.CommandLine)
+	framework.RegisterCommonFlags(flag.CommandLine)
+	framework.RegisterClusterFlags(flag.CommandLine)
+	flag.Parse()
+}
+
+func TestMain(m *testing.M) {
+	// Register test flags, then parse flags.
+	handleFlags()
+	framework.AfterReadingAllFlags(&framework.TestContext)
+
+	os.Exit(m.Run())
+}
+
+func TestE2E(t *testing.T) {
+	RunE2ETests(t)
+}
