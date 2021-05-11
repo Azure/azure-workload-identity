5fd7ae22638d108204067585cfeeea849c89dc02
Ernest Wong <chuwon@microsoft.com>
2021-05-11 15:34:15 -0700

    test: enable e2e on kind clusters and shellcheck (#24)
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -14,7 +14,7 @@ get_random_region() {
     echo "${REGIONS[${RANDOM} % ${#REGIONS[@]}]}"
 }
 
-should_create_cluster() {
+should_create_aks_cluster() {
   if [[ "${SOAK_CLUSTER:-}" == "true" ]] || [[ -n "${KUBECONFIG:-}" ]]; then
     echo "false" && return
   fi
@@ -26,22 +26,15 @@ should_create_cluster() {
 
 create_cluster_and_deploy() {
   if [[ "${LOCAL_ONLY:-}" == "true" ]]; then
-    : "${AZURE_ENVIRONMENT:?Environment variable empty or not defined.}"
-    : "${AZURE_TENANT_ID:?Environment variable empty or not defined.}"
-    : "${AZURE_SUBSCRIPTION_ID:?Environment variable empty or not defined.}"
-
-    export KIND_CLUSTER_NAME=aad-pod-managed-identity
-    make kind-delete kind-create
-
-    OUTPUT_TYPE="type=docker" make container-manager
-    kind load docker-image controller:latest --name "${KIND_CLUSTER_NAME}"
+    # create a kind cluster, then build and load the webhook manager image to the cluster
+    make kind-create
   else
     : "${REGISTRY:?Environment variable empty or not defined.}"
 
     az login -i > /dev/null && echo "Using machine identity for az commands" || echo "Using pre-existing credential for az commands"
 
     CLUSTER_NAME="${CLUSTER_NAME:-pod-managed-identity-e2e-$(openssl rand -hex 2)}"
-    if [[ "$(should_create_cluster)" == "true" ]]; then
+    if [[ "$(should_create_aks_cluster)" == "true" ]]; then
       echo "Creating an AKS cluster '${CLUSTER_NAME}'"
       az group create --name "${CLUSTER_NAME}" --location "$(get_random_region)" > /dev/null
       az aks create \
@@ -64,20 +57,22 @@ create_cluster_and_deploy() {
       echo "Granting AcrPull permission to the cluster's managed identity"
       NODE_RESOURCE_GROUP="$(az aks show --resource-group "${CLUSTER_NAME}" --name "${CLUSTER_NAME}" --query nodeResourceGroup -otsv)"
       ASSIGNEE_OBJECT_ID="$(az identity show --resource-group "${NODE_RESOURCE_GROUP}" -n "${CLUSTER_NAME}-agentpool" --query principalId -otsv)"
-      ROLE_ASSIGNMENT_ID="$(az role assignment create --assignee-object-id "${ASSIGNEE_OBJECT_ID}" --role AcrPull --scope $(az acr show --name "${REGISTRY}" --query id -otsv) --query id -otsv)"
+      ROLE_ASSIGNMENT_ID="$(az role assignment create --assignee-object-id "${ASSIGNEE_OBJECT_ID}" --role AcrPull --scope "$(az acr show --name "${REGISTRY}" --query id -otsv)" --query id -otsv)"
     fi
 
     echo "Building controller and deploying webhook to the cluster"
-    export IMG="${REGISTRY}/controller:$(git rev-parse --short HEAD)"
+    IMG="${REGISTRY}/controller:$(git rev-parse --short HEAD)"
+    export IMG
     make container-manager
   fi
 }
 
 cleanup() {
+  if [[ "${SOAK_CLUSTER:-}" == "true" ]] || [[ "${SKIP_CLEANUP:-}" == "true" ]]; then
+    return
+  fi
   if [[ "${LOCAL_ONLY:-}" == "true" ]]; then
     make kind-delete
-  fi
-  if [[ "${SOAK_CLUSTER:-}" == "true" ]]; then
     return
   fi
   if [[ -n "${ROLE_ASSIGNMENT_ID:-}" ]]; then
