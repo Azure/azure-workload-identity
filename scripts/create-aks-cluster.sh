fb7b1ce8810e503b3ff78a21406c32373cce2ece
Ernest Wong <chuwon@microsoft.com>
2021-05-26 17:12:08 -0700

    test: add an e2e job for windows containerd cluster
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

---
+++ scripts/create-aks-cluster.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${CLUSTER_NAME:?Environment variable empty or not defined.}"
+
+get_random_region() {
+    local REGIONS=("eastus" "eastus2" "westus2" "westeurope" "uksouth" "northeurope" "francecentral")
+    echo "${REGIONS[${RANDOM} % ${#REGIONS[@]}]}"
+}
+
+should_create_aks_cluster() {
+  if [[ "${SOAK_CLUSTER:-}" == "true" ]] || [[ -n "${KUBECONFIG:-}" ]]; then
+    echo "false" && return
+  fi
+  if az aks show --resource-group "${CLUSTER_NAME}" --name "${CLUSTER_NAME}" > /dev/null; then
+    echo "false" && return
+  fi
+  echo "true" && return
+}
+
+register_feature() {
+  # https://docs.microsoft.com/en-us/azure/aks/windows-container-cli#add-a-windows-server-node-pool-with-containerd-preview
+  az extension add --name aks-preview
+  az extension update --name aks-preview
+  az feature register --namespace Microsoft.ContainerService --name UseCustomizedWindowsContainerRuntime > /dev/null
+  while [[ "$(az feature list --query "[?contains(name, 'Microsoft.ContainerService/UseCustomizedWindowsContainerRuntime')].{Name:name,State:properties.state}" | jq -r '.[].State')" != "Registered" ]]; do
+    sleep 20
+  done
+  az provider register --namespace Microsoft.ContainerService
+}
+
+main() {
+  if [[ "$(should_create_aks_cluster)" == "true" ]]; then
+    echo "Creating an AKS cluster '${CLUSTER_NAME}'"
+    az group create --name "${CLUSTER_NAME}" --location "$(get_random_region)" > /dev/null
+    # TODO(chewong): ability to create an arc-enabled cluster
+    az aks create \
+      --resource-group "${CLUSTER_NAME}" \
+      --name "${CLUSTER_NAME}" \
+      --node-vm-size Standard_DS3_v2 \
+      --enable-managed-identity \
+      --network-plugin azure \
+      --kubernetes-version 1.20.5 \
+      --node-count 1 \
+      --generate-ssh-keys > /dev/null
+    if [[ "${WINDOWS_CLUSTER:-}" == "true" ]]; then
+      if [[ "${WINDOWS_CONTAINERD:-}" == "true" ]]; then
+        export -f register_feature
+        # might take around 20 minutes to register
+        timeout --foreground 1200 bash -c register_feature
+        EXTRA_ARGS="--aks-custom-headers WindowsContainerRuntime=containerd"
+      fi
+      # shellcheck disable=SC2086
+      az aks nodepool add --resource-group "${CLUSTER_NAME}" --cluster-name "${CLUSTER_NAME}" --os-type Windows --name npwin --kubernetes-version 1.20.5 --node-count 1 ${EXTRA_ARGS:-} > /dev/null
+    fi
+  fi
+}
+
+main
