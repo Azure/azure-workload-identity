e691969b72cd297eb4f7fd8ddea875b41d1bea8d
Ernest Wong <chuwon@microsoft.com>
2021-05-28 09:53:44 -0700

    test: add an e2e job for windows containerd cluster (#46)
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -12,21 +12,6 @@ readonly KUBECTL="${REPO_ROOT}/hack/tools/bin/kubectl"
 IMAGE_VERSION="$(git rev-parse --short HEAD)"
 export IMAGE_VERSION
 
-get_random_region() {
-    local REGIONS=("eastus" "eastus2" "westus2" "westeurope" "uksouth" "northeurope" "francecentral")
-    echo "${REGIONS[${RANDOM} % ${#REGIONS[@]}]}"
-}
-
-should_create_aks_cluster() {
-  if [[ "${SOAK_CLUSTER:-}" == "true" ]] || [[ -n "${KUBECONFIG:-}" ]]; then
-    echo "false" && return
-  fi
-  if az aks show --resource-group "${CLUSTER_NAME}" --name "${CLUSTER_NAME}" > /dev/null; then
-    echo "false" && return
-  fi
-  echo "true" && return
-}
-
 create_cluster() {
   if [[ "${LOCAL_ONLY:-}" == "true" ]]; then
     # create a kind cluster, then build and load the webhook manager image to the cluster
@@ -37,27 +22,7 @@ create_cluster() {
     az login -i > /dev/null && echo "Using machine identity for az commands" || echo "Using pre-existing credential for az commands"
 
     CLUSTER_NAME="${CLUSTER_NAME:-pod-managed-identity-e2e-$(openssl rand -hex 2)}"
-    if [[ "$(should_create_aks_cluster)" == "true" ]]; then
-      echo "Creating an AKS cluster '${CLUSTER_NAME}'"
-      az group create --name "${CLUSTER_NAME}" --location "$(get_random_region)" > /dev/null
-      # TODO(chewong): ability to create an arc-enabled cluster
-      az aks create \
-        --resource-group "${CLUSTER_NAME}" \
-        --name "${CLUSTER_NAME}" \
-        --node-vm-size Standard_DS3_v2 \
-        --enable-managed-identity \
-        --network-plugin azure \
-        --node-count 1 \
-        --generate-ssh-keys > /dev/null
-      if [[ "${WINDOWS_CLUSTER:-}" == "true" ]]; then
-        az aks nodepool add \
-          --resource-group "${CLUSTER_NAME}" \
-          --cluster-name "${CLUSTER_NAME}" \
-          --os-type Windows \
-          --name npwin \
-          --node-count 1 > /dev/null
-      fi
-    fi
+    "${REPO_ROOT}/scripts/create-aks-cluster.sh"
 
     # assume BYO cluster if KUBECONFIG is defined
     if [[ -z "${KUBECONFIG:-}" ]]; then
@@ -101,16 +66,20 @@ cleanup() {
 }
 trap cleanup EXIT
 
-create_cluster
-${KUBECTL} get nodes -owide
+main() {
+  create_cluster
+  ${KUBECTL} get nodes -owide
 
-make clean deploy
+  make clean deploy
 
-if [[ -n "${WINDOWS_NODE_NAME:-}" ]]; then
-  # remove the taint from the windows node we introduced above
-  ${KUBECTL} taint nodes "${WINDOWS_NODE_NAME}" kubernetes.io/os=windows:NoSchedule-
-  E2E_ARGS="--node-os-distro=windows ${E2E_ARGS:-}"
-  export E2E_ARGS
-fi
+  if [[ -n "${WINDOWS_NODE_NAME:-}" ]]; then
+    # remove the taint from the windows node we introduced above
+    ${KUBECTL} taint nodes "${WINDOWS_NODE_NAME}" kubernetes.io/os=windows:NoSchedule-
+    E2E_ARGS="--node-os-distro=windows ${E2E_ARGS:-}"
+    export E2E_ARGS
+  fi
 
-make test-e2e-run
+  make test-e2e-run
+}
+
+main
