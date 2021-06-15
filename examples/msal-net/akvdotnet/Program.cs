e3eb96c2636344ad9065b55d68135865cc1aac06
Anish Ramasekar <anish.ramasekar@gmail.com>
2021-06-14 17:02:47 -0700

    chore: update to using Azure.Security.KeyVault.Secrets for example (#65)
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- examples/msal-net/akvdotnet/Program.cs
+++ examples/msal-net/akvdotnet/Program.cs
@@ -1,10 +1,7 @@
 // <directives>
-using Microsoft.Identity.Client;
 using System;
 using System.Threading;
-using System.Collections.Generic;
-using Microsoft.Azure.KeyVault;
-using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
 // <directives>
 
 namespace akvdotnet
@@ -20,81 +17,21 @@ namespace akvdotnet
             // keyvault URL
             string keyvaultURL = "https://" + keyvaultName + ".vault.azure.net/";
 
-            KeyVaultClient kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(P.AcquireTokenUsingMSAL));
+            SecretClient client = new SecretClient(
+                new Uri(keyvaultURL),
+                new MyClientAssertionCredential());
 
             while (true)
             {
                 Console.WriteLine($"{Environment.NewLine}START {DateTime.UtcNow} ({Environment.MachineName})");
 
-                var fetchedSecret = P.GetSecret(kvClient, keyvaultURL, secretName);
-                var secretValue = fetchedSecret.Result;
-                Console.WriteLine("Your secret is " + secretValue);
+                // <getsecret>                
+                var keyvaultSecret = client.GetSecret(secretName).Value;
+                Console.WriteLine("Your secret is " + keyvaultSecret.Value);
 
                 // sleep and retry periodically
                 Thread.Sleep(600000);
             }
-        }
-
-        public async Task<string> GetSecret(KeyVaultClient kvClient, string kvURL, string secretName)
-        {
-            // <getsecret>                
-            var keyvaultSecret = await kvClient.GetSecretAsync($"{kvURL}", secretName);
-            return keyvaultSecret.Value;
-        }
-
-        public async Task<string> AcquireTokenUsingMSAL(string authority, string resource, string scope)
-        {
-            // <authentication>
-            // AAD Pod Identity webhook will inject the following env vars
-            // 	AZURE_CLIENT_ID with the clientID set in the service account annotation
-            // 	AZURE_TENANT_ID with the tenantID set in the service account annotation. If not defined, then
-            // 		the tenantID provided via aad-pi-webhook-config for the webhook will be used.
-            // 	TOKEN_FILE_PATH is the service account token path
-            var clientID = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
-            var tokenPath = Environment.GetEnvironmentVariable("TOKEN_FILE_PATH");
-            var tenantID = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
-
-            string[] _scopes = new string[] { resource + "/.default" };
-
-            // read the service account token from the filesystem
-            string signedClientAssertion = ReadJWTFromFS(tokenPath);
-
-            // TODO (aramase) remove query params after available in prod
-            Dictionary<string, string> otherParams = new Dictionary<string, string>();
-            otherParams.Add("dc", "ESTS-PUB-WUS2-AZ1-FD000-TEST1");
-            otherParams.Add("fiextoidc", "true");
-
-            AuthenticationResult authResult = null;
-            IConfidentialClientApplication confidentialClient;
-            confidentialClient = ConfidentialClientApplicationBuilder.Create(clientID)
-                .WithAuthority(authority)
-                .WithClientAssertion(signedClientAssertion)
-                .WithTenantId(tenantID).Build();
-
-            try
-            {
-                authResult = await confidentialClient.AcquireTokenForClient(_scopes)
-                                .WithExtraQueryParameters(otherParams)
-                                .ExecuteAsync();
-            }
-            catch (MsalUiRequiredException ex)
-            {
-                // The application doesn't have sufficient permissions.
-                // - Did you declare enough app permissions during app creation?
-                // - Did the tenant admin grant permissions to the application?
-            }
-            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
-            {
-                // Invalid scope. The scope has to be in the form "https://resourceurl/.default"
-                // Mitigation: Change the scope to be as expected.
-            }
-            return authResult.AccessToken;
-        }
-
-        public string ReadJWTFromFS(string tokenPath)
-        {
-            string text = System.IO.File.ReadAllText(tokenPath);
-            return text;
         }
     }
 }
---
+++ examples/msal-net/akvdotnet/TokenCredential.cs
@@ -0,0 +1,63 @@
+// <directives>
+using Azure.Core;
+using System;
+using Microsoft.Identity.Client;
+using System.Threading;
+using System.Threading.Tasks;
+// <directives>
+
+public class MyClientAssertionCredential : TokenCredential
+{
+    private readonly IConfidentialClientApplication _confidentialClientApp;
+
+    public MyClientAssertionCredential()
+    {
+        // <authentication>
+        // AAD Pod Identity webhook will inject the following env vars
+        // 	AZURE_CLIENT_ID with the clientID set in the service account annotation
+        // 	AZURE_TENANT_ID with the tenantID set in the service account annotation. If not defined, then
+        // 		the tenantID provided via aad-pi-webhook-config for the webhook will be used.
+        // 	TOKEN_FILE_PATH is the service account token path
+        var clientID = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
+        var tokenPath = Environment.GetEnvironmentVariable("TOKEN_FILE_PATH");
+        var tenantID = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
+
+        _confidentialClientApp = ConfidentialClientApplicationBuilder.Create(clientID)
+                .WithClientAssertion(ReadJWTFromFS(tokenPath))
+                .WithTenantId(tenantID).Build();
+    }
+
+    public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        return GetTokenAsync(requestContext, cancellationToken).GetAwaiter().GetResult();
+    }
+
+    public override async ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        AuthenticationResult result = null;
+        try
+        {
+            result = await _confidentialClientApp
+                        .AcquireTokenForClient(requestContext.Scopes)
+                        .ExecuteAsync();
+        }
+        catch (MsalUiRequiredException ex)
+        {
+            // The application doesn't have sufficient permissions.
+            // - Did you declare enough app permissions during app creation?
+            // - Did the tenant admin grant permissions to the application?
+        }
+        catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS70011"))
+        {
+            // Invalid scope. The scope has to be in the form "https://resourceurl/.default"
+            // Mitigation: Change the scope to be as expected.
+        }
+        return new AccessToken(result.AccessToken, result.ExpiresOn);
+    }
+
+    public string ReadJWTFromFS(string tokenPath)
+    {
+        string text = System.IO.File.ReadAllText(tokenPath);
+        return text;
+    }
+}
