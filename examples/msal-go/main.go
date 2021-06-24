c996a82e2051c81e72b680232cae730bb3714bf5
Anish Ramasekar <anish.ramasekar@gmail.com>
2021-06-24 16:24:11 -0700

    feat: implement custom authorizer for msal-go example
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- examples/msal-go/main.go
+++ examples/msal-go/main.go
@@ -5,56 +5,24 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
+	"github.com/Azure/go-autorest/autorest"
 	"k8s.io/klog/v2"
 )
 
 func main() {
-	ctx := context.Background()
+	keyvaultName := os.Getenv("KEYVAULT_NAME")
+	secretName := os.Getenv("SECRET_NAME")
 
-	// AAD Pod Identity webhook will inject the following env vars
-	// 	AZURE_CLIENT_ID with the clientID set in the service account annotation
-	// 	AZURE_TENANT_ID with the tenantID set in the service account annotation. If not defined, then
-	// 		the tenantID provided via aad-pi-webhook-config for the webhook will be used.
-	// 	TOKEN_FILE_PATH is the service account token path
+	keyvaultURL := fmt.Sprintf("https://%s.vault.azure.net/", keyvaultName)
 
-	tokenFilePath := os.Getenv("TOKEN_FILE_PATH")
-	tenantID := os.Getenv("AZURE_TENANT_ID")
-	// if the service account wasn't annotated with the clientID, then this will be empty
-	// ensure to set your clientID if not provided through annotation
-	clientID := os.Getenv("AZURE_CLIENT_ID")
+	// initialize keyvault client with custom authorizer
+	kvClient := keyvault.New()
+	kvClient.Authorizer = autorest.NewBearerAuthorizerCallback(nil, clientAssertionBearerAuthorizerCallback)
 
-	// read the service account token from the filesystem
-	signedAssertion, err := readJWTFromFS(tokenFilePath)
+	secretBundle, err := kvClient.GetSecret(context.Background(), keyvaultURL, secretName, "")
 	if err != nil {
-		klog.Fatalf("failed to read service account token: %v", err)
+		klog.Fatalf("failed to get secret from keyvault, err: %+v", err)
 	}
-
-	cred, err := confidential.NewCredFromAssertion(signedAssertion)
-	if err != nil {
-		klog.Fatalf("failed to create confidential creds: %v", err)
-	}
-
-	// create the confidential client to request an AAD token
-	confidentialClientApp, err := confidential.New(clientID, cred,
-		// TODO (aramase) remove query params after available in prod
-		confidential.WithAuthority(fmt.Sprintf("https://login.microsoftonline.com/%s/oauth2/token?dc=ESTS-PUB-WUS2-AZ1-FD000-TEST1&fiextoidc=true", tenantID)))
-	if err != nil {
-		klog.Fatalf("failed to create confidential client app: %v", err)
-	}
-
-	scopes := []string{"https://graph.microsoft.com/.default"}
-	result, err := confidentialClientApp.AcquireTokenByCredential(ctx, scopes)
-	if err != nil {
-		klog.Fatalf("failed to get token: %v", err)
-	}
-	klog.InfoS("successfully obtained the token", "token", result.AccessToken, "expiry", result.ExpiresOn)
-}
-
-func readJWTFromFS(tokenFilePath string) (string, error) {
-	token, err := os.ReadFile(tokenFilePath)
-	if err != nil {
-		return "", err
-	}
-	return string(token), nil
+	klog.InfoS("successfully got secret", "secret", *secretBundle.Value)
 }
