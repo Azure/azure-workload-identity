fb21ee1dadcd7a83d9b6524df31604a6f47fd3bf
Heba Elayoty <31887807+helayoty@users.noreply.github.com>
2021-07-07 19:37:42 -0700

    generate helm from manifest (#66)

---
+++ third_party/open-policy-agent/gatekeeper/helmify/replacements.go
@@ -0,0 +1,21 @@
+package main
+
+var replacements = map[string]string{
+	`HELMSUBST_DEPLOYMENT_CONTAINER_RESOURCES: ""`: `{{- toYaml .Values.resources | nindent 10 }}`,
+
+	`HELMSUBST_DEPLOYMENT_NODE_SELECTOR: ""`: `{{- toYaml .Values.nodeSelector | nindent 8 }}`,
+
+	"HELMSUBST_DEPLOYMENT_REPLICAS": `{{ .Values.replicaCount }}`,
+
+	`HELMSUBST_DEPLOYMENT_AFFINITY: ""`: `{{- toYaml .Values.affinity | nindent 8 }}`,
+
+	`HELMSUBST_DEPLOYMENT_TOLERATIONS: ""`: `{{- toYaml .Values.tolerations | nindent 8 }}`,
+
+	"HELMSUBST_CONFIGMAP_AZURE_ENVIRONMENT": `{{ .Values.azureEnvironment | default "AzurePublicCloud" }}`,
+
+	"HELMSUBST_CONFIGMAP_AZURE_TENANT_ID": `{{ required "A valid .Values.azureTenantID entry required!" .Values.azureTenantID }}`,
+
+	`HELMSUBST_SERVICE_TYPE: ""`: `{{- if .Values.service }}
+  type: {{  .Values.service.type | default "ClusterIP" }}
+  {{- end }}`,
+}
