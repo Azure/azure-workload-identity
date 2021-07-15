63d6ad453e79367e3fa9a17a93c3c6aaeb13f8ea
Anish Ramasekar <anish.ramasekar@gmail.com>
2021-07-15 13:29:20 -0400

    feat: add user agent to client
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

---
+++ pkg/version/version.go
@@ -0,0 +1,20 @@
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var (
+	// Vcs is the commit hash for the binary build
+	Vcs string
+	// BuildTime is the date for the binary build
+	BuildTime string
+	// BuildVersion is the aad-pod-managed-identity version. Will be overwritten from build.
+	BuildVersion string
+)
+
+// GetUserAgent returns a user agent of the format: aad-pod-managed-identity/<version> (<goos>/<goarch>) <vcs>/<timestamp>
+func GetUserAgent() string {
+	return fmt.Sprintf("aad-pod-managed-identity/%s (%s/%s) %s/%s", BuildVersion, runtime.GOOS, runtime.GOARCH, Vcs, BuildTime)
+}
