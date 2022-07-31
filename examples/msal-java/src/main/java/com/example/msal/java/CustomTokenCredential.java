e7dae2215d9cd2b553d2b88dff427afbde957933
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2022-07-31 22:43:56 +0100

    docs: improve reactive code in Java sample
    Calling .join() on a CompletableFuture is a blocking operation.  In a method that returns Mono it is better to convert the future to a Mono and then chain onto that operation's completion, rather than to block the calling thread and then return an already-completed Mono.
    
    Signed-off-by: Ian Roberts <i.roberts@dcs.shef.ac.uk>

--- examples/msal-java/src/main/java/com/example/msal/java/CustomTokenCredential.java
+++ examples/msal-java/src/main/java/com/example/msal/java/CustomTokenCredential.java
@@ -15,41 +15,37 @@ import com.azure.core.credential.TokenRequestContext;
 import com.microsoft.aad.msal4j.ClientCredentialFactory;
 import com.microsoft.aad.msal4j.ClientCredentialParameters;
 import com.microsoft.aad.msal4j.ConfidentialClientApplication;
-import com.microsoft.aad.msal4j.IAuthenticationResult;
 import com.microsoft.aad.msal4j.IClientCredential;
 
 import reactor.core.publisher.Mono;
 
 public class CustomTokenCredential implements TokenCredential {
-    public Mono<AccessToken> getToken(TokenRequestContext request) {
+    private final ConfidentialClientApplication app;
+    
+    public CustomTokenCredential() {
         Map<String, String> env = System.getenv();
         String clientAssertion;
         try {
             clientAssertion = new String(Files.readAllBytes(Paths.get(env.get("AZURE_FEDERATED_TOKEN_FILE"))),
                     StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        IClientCredential credential = ClientCredentialFactory.createFromClientAssertion(clientAssertion);
-        String authority = env.get("AZURE_AUTHORITY_HOST") + env.get("AZURE_TENANT_ID");
-        try {
-            ConfidentialClientApplication app = ConfidentialClientApplication
-                    .builder(env.get("AZURE_CLIENT_ID"), credential).authority(authority).build();
-
-            Set<String> scopes = new HashSet<>();
-            for (String scope : request.getScopes())
-                scopes.add(scope);
-
-            ClientCredentialParameters parameters = ClientCredentialParameters.builder(scopes).build();
-            IAuthenticationResult result = app.acquireToken(parameters).join();
-            return Mono.just(
-                    new AccessToken(result.accessToken(), result.expiresOnDate().toInstant().atOffset(ZoneOffset.UTC)));
+            IClientCredential credential = ClientCredentialFactory.createFromClientAssertion(clientAssertion);
+            String authority = env.get("AZURE_AUTHORITY_HOST") + env.get("AZURE_TENANT_ID");
+            app = ConfidentialClientApplication.builder(env.get("AZURE_CLIENT_ID"), credential)
+                    .authority(authority).build();
         } catch (Exception e) {
             System.out.printf("Error creating client application: %s", e.getMessage());
-            System.exit(1);
+            throw new RuntimeException(e);
         }
+    }
+    
+    public Mono<AccessToken> getToken(TokenRequestContext request) {
+        Set<String> scopes = new HashSet<>();
+        for (String scope : request.getScopes())
+            scopes.add(scope);
 
-        return Mono.empty();
+        ClientCredentialParameters parameters = ClientCredentialParameters.builder(scopes).build();
+        return Mono.defer(() -> Mono.fromFuture(app.acquireToken(parameters))).map((result) ->
+                new AccessToken(result.accessToken(), result.expiresOnDate().toInstant().atOffset(ZoneOffset.UTC)));
     }
 }
