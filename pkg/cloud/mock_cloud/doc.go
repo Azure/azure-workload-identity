d5ffd3fd59572b815017f8f60b5d5b10796c8e16
Anish Ramasekar <anish.ramasekar@gmail.com>
2022-08-15 15:13:28 -0700

    chore: update to go 1.19 (#531)
    * chore: update to go 1.19
    
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>
    
    * chore: bump golangci-lint to v1.48.0
    
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>
    
    * chore: remove ioutil and run gofmt
    
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>
    
    * chore: use 1.19 to pin minor version
    
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>
    
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- pkg/cloud/mock_cloud/doc.go
+++ pkg/cloud/mock_cloud/doc.go
@@ -1,3 +1,4 @@
 // Run go generate to regenerate this mock.
+//
 //go:generate ../../../hack/tools/bin/mockgen -destination cloud_mock.go -package mock_cloud -source ../azureclient.go
 package mock_cloud //nolint
