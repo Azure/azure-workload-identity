b1a2be9e6abbe8d0dc3b5bf43c3e00a39bd5dc82
Anish Ramasekar <anish.ramasekar@gmail.com>
2022-11-17 01:02:03 +0000

    chore: update msal-go example to use azidentity and azsecrets sdk
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- examples/msal-go/main.go
+++ examples/msal-go/main.go
@@ -5,8 +5,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
-	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
 	"k8s.io/klog/v2"
 )
 
@@ -20,12 +19,43 @@ func main() {
 		klog.Fatal("SECRET_NAME environment variable is not set")
 	}
 
-	// initialize keyvault client with custom authorizer
-	kvClient := keyvault.New()
-	kvClient.Authorizer = autorest.NewBearerAuthorizerCallback(nil, clientAssertionBearerAuthorizerCallback)
+	// Azure AD Workload Identity webhook will inject the following env vars
+	// 	AZURE_CLIENT_ID with the clientID set in the service account annotation
+	// 	AZURE_TENANT_ID with the tenantID set in the service account annotation. If not defined, then
+	// 	the tenantID provided via azure-wi-webhook-config for the webhook will be used.
+	// 	AZURE_FEDERATED_TOKEN_FILE is the service account token path
+	// 	AZURE_AUTHORITY_HOST is the AAD authority hostname
+	clientID := os.Getenv("AZURE_CLIENT_ID")
+	tenantID := os.Getenv("AZURE_TENANT_ID")
+	tokenFilePath := os.Getenv("AZURE_FEDERATED_TOKEN_FILE")
+	authorityHost := os.Getenv("AZURE_AUTHORITY_HOST")
+
+	if clientID == "" {
+		klog.Fatal("AZURE_CLIENT_ID environment variable is not set")
+	}
+	if tenantID == "" {
+		klog.Fatal("AZURE_TENANT_ID environment variable is not set")
+	}
+	if tokenFilePath == "" {
+		klog.Fatal("AZURE_FEDERATED_TOKEN_FILE environment variable is not set")
+	}
+	if authorityHost == "" {
+		klog.Fatal("AZURE_AUTHORITY_HOST environment variable is not set")
+	}
+
+	cred, err := newClientAssertionCredential(tenantID, clientID, authorityHost, tokenFilePath, nil)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	// initialize keyvault client
+	client, err := azsecrets.NewClient(keyvaultURL, cred, &azsecrets.ClientOptions{})
+	if err != nil {
+		klog.Fatal(err)
+	}
 
 	for {
-		secretBundle, err := kvClient.GetSecret(context.Background(), keyvaultURL, secretName, "")
+		secretBundle, err := client.GetSecret(context.Background(), secretName, "", nil)
 		if err != nil {
 			klog.ErrorS(err, "failed to get secret from keyvault", "keyvault", keyvaultURL, "secretName", secretName)
 			os.Exit(1)
