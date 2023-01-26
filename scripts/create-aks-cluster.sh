0ace9ecb69f10438ad6abe91970a8859d6e2cb60
Anish Ramasekar <anish.ramasekar@gmail.com>
2023-01-26 04:12:48 -0800

    ci: update kubernetes version to v1.24 for aks (#718)
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- scripts/create-aks-cluster.sh
+++ scripts/create-aks-cluster.sh
@@ -42,8 +42,8 @@ main() {
     timeout --foreground 1200 bash -c register_feature
     echo "Creating an AKS cluster '${CLUSTER_NAME}'"
     LOCATION="$(get_random_region)"
-    # get the latest patch version of 1.23
-    KUBERNETES_VERSION="$(az aks get-versions --location "${LOCATION}" --query 'orchestrators[*].orchestratorVersion' -otsv | grep '1.23' | tail -1)"
+    # get the latest patch version of 1.24
+    KUBERNETES_VERSION="$(az aks get-versions --location "${LOCATION}" --query 'orchestrators[*].orchestratorVersion' -otsv | grep '1.24' | tail -1)"
     az group create --name "${CLUSTER_NAME}" --location "${LOCATION}" > /dev/null
     # TODO(chewong): ability to create an arc-enabled cluster
     az aks create \
