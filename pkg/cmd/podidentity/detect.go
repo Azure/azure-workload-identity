396affa54b3224d018f460548e3f1f5a40799561
aramase <aramase@users.noreply.github.com>
2024-04-17 19:28:40 +0000

    release: update manifest and helm charts for v1.2.2

--- pkg/cmd/podidentity/detect.go
+++ pkg/cmd/podidentity/detect.go
@@ -35,7 +35,7 @@ var (
 
 const (
 	imageRepository = "mcr.microsoft.com/oss/azure/workload-identity"
-	imageTag        = "v1.2.1"
+	imageTag        = "v1.2.2"
 
 	proxyInitImageName     = "proxy-init"
 	proxyImageName         = "proxy"
